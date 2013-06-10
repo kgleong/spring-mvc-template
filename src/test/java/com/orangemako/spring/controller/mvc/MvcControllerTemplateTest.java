75598be78bf51e8a09d59872f25ad481ea43d54b
Kevin Leong <kgleong@gmail.com>
2013-06-10 05:18:22 -0700

    update class names in MvcControllerTemplateTest

--- src/test/java/com/orangemako/spring/controller/mvc/MvcControllerTemplateTest.java
+++ src/test/java/com/orangemako/spring/controller/mvc/MvcControllerTemplateTest.java
@@ -9,19 +9,19 @@ import static org.testng.Assert.*;
  *
  * @author Kevin Leong
  */
-public class MvcTemplateTest {
+public class MvcControllerTemplateTest {
 
     @Test
     public void testSum() throws Exception {
-        MvcTemplate mvcTemplate = new MvcTemplate();
+        MvcControllerTemplate mvcController = new MvcControllerTemplate();
 
         int numOne = 5;
         int numTwo = 6;
 
-        assertEquals(mvcTemplate.sum(numOne, numTwo), 11);
+        assertEquals(mvcController.sum(numOne, numTwo), 11);
 
         numOne = 111;
         numTwo = 5023;
-        assertEquals(mvcTemplate.sum(numOne, numTwo), 5134);
+        assertEquals(mvcController.sum(numOne, numTwo), 5134);
     }
 }

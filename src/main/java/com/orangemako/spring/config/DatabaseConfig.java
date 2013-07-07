2465528d776e49d08984a728bf5ed6416aa07746
Kevin Leong <kgleong@gmail.com>
2013-07-07 09:22:59 -0700

    add database configuration context

---
+++ src/main/java/com/orangemako/spring/config/DatabaseConfig.java
@@ -0,0 +1,17 @@
+package com.orangemako.spring.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Database Configuration.
+ *
+ * @author Kevin Leong
+ */
+@Configuration
+public class DatabaseConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(DatabaseConfig.class);
+
+
+}

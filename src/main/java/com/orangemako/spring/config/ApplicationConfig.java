17f267985e8df16675997bd0c97d69a6d967e5a0
Kevin Leong <kgleong@gmail.com>
2013-07-07 09:27:09 -0700

    put database configuratoin in databaseconfig context

--- src/main/java/com/orangemako/spring/config/ApplicationConfig.java
+++ src/main/java/com/orangemako/spring/config/ApplicationConfig.java
@@ -1,19 +1,13 @@
 package com.orangemako.spring.config;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
 
 /**
  * Application Context Configuration.
@@ -21,75 +15,11 @@ import java.beans.PropertyVetoException;
  * @author Kevin Leong
  */
 @Configuration // Identifies this class to Spring as a configuration file
+@Import(DatabaseConfig.class)
 @MapperScan(basePackages = {"com.orangemako.spring.dao"}) // Scans the specified package(es) for MyBatis mapper interfaces.
 @ComponentScan(basePackages = {"com.orangemako.spring.service"}) // Scans the following packages for classes with @Component annotations
 public class ApplicationConfig {
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationConfig.class);
-
-    // JDBC Connection values from property file(s)
-    @Value("${jdbc.driver}")
-    String jdbcDriver;
-
-    @Value("${jdbc.url}")
-    String jdbcUrl;
-
-    @Value("${jdbc.username}")
-    String jdbcUsername;
-
-    @Value("${jdbc.password}")
-    String jdbcPassword;
-
-    // Database pool configuration
-    @Value("${db.max.pool.size}")
-    int maxPoolSize;
-
-    @Value("${db.min.pool.size}")
-    int minPoolSize;
-
-    @Value("${db.max.statements.per.connection}")
-    int maxStatementsPerConnection;
-
-    @Value("${db.acquire.retry.attempts}")
-    int acquireRetryAttempts;
-
-    @Value("${db.acquire.retry.delay}")
-    int acquireRetryDelay;
-
-    /**
-     * Setup a pooled database connection with a remote database.
-     *
-     * @return
-     */
-    DataSource remoteDatabase() throws PropertyVetoException {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-
-        dataSource.setDriverClass(jdbcDriver);
-        dataSource.setJdbcUrl(jdbcUrl);
-        dataSource.setUser(jdbcUsername);
-        dataSource.setPassword(jdbcPassword);
-
-        dataSource.setMaxPoolSize(maxPoolSize);
-        dataSource.setMinPoolSize(minPoolSize);
-        dataSource.setMaxStatementsPerConnection(maxStatementsPerConnection);
-
-        dataSource.setAcquireRetryAttempts(acquireRetryAttempts);
-        dataSource.setAcquireRetryDelay(acquireRetryDelay);
-
-        return dataSource;
-    }
-
-    /**
-     * Pass the database connection to MyBatis
-     * @return
-     * @throws Exception
-     */
-    @Bean
-    SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(remoteDatabase());
-
-        return factoryBean.getObject();
-    }
 
     /**
      * Read in properties from property files.
--- src/main/java/com/orangemako/spring/config/DatabaseConfig.java
+++ src/main/java/com/orangemako/spring/config/DatabaseConfig.java
@@ -1,8 +1,17 @@
 package com.orangemako.spring.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 /**
  * Database Configuration.
@@ -13,5 +22,77 @@ import org.springframework.context.annotation.Configuration;
 public class DatabaseConfig {
     private static final Logger LOG = LoggerFactory.getLogger(DatabaseConfig.class);
 
+    // JDBC Connection values from property file(s)
+    @Value("${jdbc.driver}")
+    String jdbcDriver;
 
+    @Value("${jdbc.url}")
+    String jdbcUrl;
+
+    @Value("${jdbc.username}")
+    String jdbcUsername;
+
+    @Value("${jdbc.password}")
+    String jdbcPassword;
+
+    // Database pool configuration
+    @Value("${db.max.pool.size}")
+    int maxPoolSize;
+
+    @Value("${db.min.pool.size}")
+    int minPoolSize;
+
+    @Value("${db.max.statements.per.connection}")
+    int maxStatementsPerConnection;
+
+    @Value("${db.acquire.retry.attempts}")
+    int acquireRetryAttempts;
+
+    @Value("${db.acquire.retry.delay}")
+    int acquireRetryDelay;
+
+    /**
+     * Setup a pooled database connection with a remote database.
+     *
+     * @return
+     */
+    DataSource remoteDatabase() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+        dataSource.setDriverClass(jdbcDriver);
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUser(jdbcUsername);
+        dataSource.setPassword(jdbcPassword);
+
+        dataSource.setMaxPoolSize(maxPoolSize);
+        dataSource.setMinPoolSize(minPoolSize);
+        dataSource.setMaxStatementsPerConnection(maxStatementsPerConnection);
+
+        dataSource.setAcquireRetryAttempts(acquireRetryAttempts);
+        dataSource.setAcquireRetryDelay(acquireRetryDelay);
+
+        return dataSource;
+    }
+
+    /**
+     * Pass the database connection to MyBatis
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(remoteDatabase());
+
+        return factoryBean.getObject();
+    }
+
+    /**
+     * Read in properties from property files.
+     * @return
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return ConfigUtils.propertySourcesPlaceholderConfigurer();
+    }
 }
